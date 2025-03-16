9e981e31dbf6aab60ae2957d8f1492e7cbb185bd
action <action@github.com>
2025-03-16 02:15:38 +0000

    Update

--- root.sh
+++ root.sh
@@ -1,6 +1,6 @@
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
-sed -i 's/#Port 22/Port 21318/g' /etc/ssh/sshd_config
+sed -i 's/#Port 22/Port 51655/g' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
