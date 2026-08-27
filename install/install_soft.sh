ba2788f1c7094ce06e7d050f7cb58f36d31fb6a5
action <action@github.com>
2026-08-27 10:10:40 +0000

    Update

---
+++ install/install_soft.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+mtype=$1
+actionType=$2
+name=$3
+version=$4
+. /www/server/panel/install/public.sh
+serverUrl=$NODE_URL/install
+
+if [ ! -f 'lib.sh' ];then
+	wget -O lib.sh $serverUrl/$mtype/lib.sh
+fi
+
+libNull=`cat lib.sh`
+if [ "$libNull" == '' ];then
+	wget -O lib.sh $serverUrl/$mtype/lib.sh
+fi
+
+sed -i "s/http:\/\/download.bt.cn\/install\/public.sh/https:\/\/cdn.jsdelivr.net\/gh\/smsony\/bt\/install\/public.sh/" lib.sh
+
+wget -O $name.sh $serverUrl/$mtype/$name.sh
+sed -i 's|\\cp -rpa|echo skipped #\\cp -rpa|' $name.sh
+#sed -i '/Tpublic.sh/d' $name.sh
+sed -i 's|wget -O ${checkFile}|echo skipped #wget -O ${checkFile}|' $name.sh
+
+if [ "$actionType" == 'install' ];then
+	bash lib.sh
+fi
+
+bash $name.sh $actionType $version
+echo '|-Successify --- 命令已执行! ---'
